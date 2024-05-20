b2501c153272e60510e661eb2494d82d65ebd31a
nandita1207 <nanditabajaj804@gmail.com>
2024-05-20 10:42:03 -0400

    vpc detroy function added

--- aws_manager/nandita_1_vpc.py
+++ aws_manager/nandita_1_vpc.py
@@ -11,3 +11,36 @@ def create_vpc(config):
         return vpc
     except Exception as error:
         print(f"Failed to create VPC. Error: {error}")
+
+
+def delete_vpc(vpc_id):
+    ec2_client = boto3.client('ec2')
+    try:
+        # Detach and delete all associated resources like internet gateways
+        igws = ec2_client.describe_internet_gateways(Filters=[{'Name': 'attachment.vpc-id', 'Values': [vpc_id]}])
+        for igw in igws['InternetGateways']:
+            ec2_client.detach_internet_gateway(InternetGatewayId=igw['InternetGatewayId'], VpcId=vpc_id)
+            ec2_client.delete_internet_gateway(InternetGatewayId=igw['InternetGatewayId'])
+        
+        # Delete subnets
+        subnets = ec2_client.describe_subnets(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
+        for subnet in subnets['Subnets']:
+            ec2_client.delete_subnet(SubnetId=subnet['SubnetId'])
+
+        # Delete security groups
+        sgs = ec2_client.describe_security_groups(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
+        for sg in sgs['SecurityGroups']:
+            if sg['GroupName'] != 'default':
+                ec2_client.delete_security_group(GroupId=sg['GroupId'])
+
+        # Delete route tables
+        rts = ec2_client.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
+        for rt in rts['RouteTables']:
+            if not rt['Associations']:  # Avoid deleting the main route table
+                ec2_client.delete_route_table(RouteTableId=rt['RouteTableId'])
+
+        # Finally delete the VPC
+        ec2_client.delete_vpc(VpcId=vpc_id)
+        print(f"Deleted VPC with ID: {vpc_id}")
+    except Exception as error:
+        print(f"Failed to delete VPC. Error: {error}")
